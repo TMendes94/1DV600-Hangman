846940942fa06f343fca6a3f8c6ca59373b8da89
Tomas M <38334432+TMendes94@users.noreply.github.com>
2019-03-08 22:22:21 +0100

    Update GameTest.java

--- src/Model/GameTest.java
+++ src/Model/GameTest.java
@@ -1,29 +1,89 @@
 package Model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-	Game g = new Game();
-	private List<String> list1;
-	private List<String> list2;
-
-	@Before
-	void setup() {
-		list1 = Arrays.asList("substitute", "hemisphere", "intervention", "transmission",
+	
+	@Test
+	void testSetWordAssertEquals() {
+		Game g = new Game();
+		List<String>list1 = Arrays.asList("substitute", "hemisphere", "intervention", "transmission", 
 				"remunerate");
-		list2 = Arrays.asList("brink", "golf", "listen", "free", "banana");
+		List<String>list2 = Arrays.asList("brink", "golf", "listen", "free", "banana");
+		List<String>list3 = new ArrayList<String>();
+		
+		System.out.print("Word is: ");
+		String testList1 = g.setWord(list2);
+		assertEquals(testList1,g.getWord());
+		
+		System.out.print("Word is: ");
+		String testList2 = g.setWord(list1);
+		assertEquals(testList2, g.getWord());
+		
+		System.out.print("Word is: ");
+		list3.add("divide");
+		list3.add("subtraction");
+		list3.add("addition");
+		String testList3 = g.setWord(list3);
+		assertEquals(testList3, g.getWord());
 	}
 	
 	@Test
-	void testSetWord() {
-		String test = g.setWord(list1);
-		assertTrue(list1.isEmpty());
+	void testSetWordNullList() {
+		try {
+			Game g = new Game();
+		
+		List<String>list1 = null;		
+		String testList1 = g.setWord(list1);
+		assertNull(testList1,g.getWord());
+		}
+		catch (NullPointerException npe) {
+			npe.toString();
+		}
+		
 	}
-
+	
+	@Test
+	void testFillWithDashAssertTrue() {
+		Game g = new Game();
+		String stringToTest = "test";
+		List<Character> characterList = new ArrayList<Character>();
+		characterList.add('_');
+		characterList.add('_');
+		characterList.add('_');
+		characterList.add('_');
+		g.fillWithDash(stringToTest);
+		assertTrue(g.correctLetter.equals(characterList));
+	}
+	
+	@Test
+	void testFillWithDashAssertFalse() {
+		Game g = new Game();
+		String stringToTest = "testing";
+		List<Character> charList = new ArrayList<Character>();
+		charList.add('_');
+		charList.add('_');
+		charList.add('_');
+		charList.add('_');
+		charList.add('_');
+		charList.add('_');
+		g.fillWithDash(stringToTest);
+		assertFalse(g.correctLetter.equals(charList));
+	}
+	
+	@Test
+	void testIsWordPicked() {
+		Game g = new Game();
+		List<String>list1 = Arrays.asList("brink", "golf", "listen", "free", "banana");
+		g.setWord(list1);
+		assertFalse(g.isWordPicked());//false assertion even if the word is picked since our method is
+									  // not implemented yet
+	}
 }
